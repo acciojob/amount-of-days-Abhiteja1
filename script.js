d634d23fbd655f3cfd7b7bf092298f6096a8febf
Dumpala Abhiteja <125918097+Abhiteja1@users.noreply.github.com>
2023-11-17 20:00:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 //your JS code here. If required.
 function daysOfAYear(year){
-	if((year%4==0)&&(year%100!==0)){
-		return 366;
-	}
-	else if(year%400==0){
+	if((year%4==0)&&(year%100!==0)||(year%400==0)){
 		return 366;
 	}
 	else{
 		return 365;
 	}
-	  
+ 
 }
 let year = prompt("enter year: ");
 alert(daysOfAYear(year));
